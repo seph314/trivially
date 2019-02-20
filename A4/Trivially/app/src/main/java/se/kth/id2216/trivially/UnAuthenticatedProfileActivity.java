7748dd289e2eea5f8f6322772f8e3108fe12aa4c
Anders <andpet52@kth.se>
2019-02-20 09:08:42 +0100

    stuck in a loop when logging in

--- A4/Trivially/app/src/main/java/se/kth/id2216/trivially/UnAuthenticatedProfileActivity.java
+++ A4/Trivially/app/src/main/java/se/kth/id2216/trivially/UnAuthenticatedProfileActivity.java
@@ -5,25 +5,38 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 
-public class UnAuthenticatedProfileActivity extends Activity implements
-        View.OnClickListener {
+public class UnAuthenticatedProfileActivity extends Activity implements View.OnClickListener {
 
     SharedPreferences sharedPrefs;
 
-    // declare_auth
+    private static final String TAG = "AuthenticateActivityTAG";
+    private static final int RC_SIGN_IN = 9001;
+
     private FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
     private GoogleSignInClient mGoogleSignInClient;
 
 
@@ -31,35 +44,53 @@ public class UnAuthenticatedProfileActivity extends Activity implements
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_un_authenticated_profile);
-
         sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        if(sharedPrefs.getBoolean("authenticated",false)){
-            redirectToAuthenticatedProfile();
-        }
-        else
-            setupActivity();
+
+        // Configure Auth Listener
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null){
+                    startActivity(new Intent(UnAuthenticatedProfileActivity.this, AuthenticatedProfileActivity.class)); // send user to logged in page
+                }
+            }
+        };
 
         // Button listeners
         findViewById(R.id.signInButton).setOnClickListener(this);
-        findViewById(R.id.logOutBtn).setOnClickListener(this);
 
-        // [START config_signin]
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken(getString(R.string.client_id)) //from Google API and our keystore
                 .requestEmail()
                 .build();
-        // [END config_signin]
 
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
+        setupActivity();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // the mAuthListener keeps track if a user is logged in or not
+        mAuth.addAuthStateListener(mAuthListener);
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+
     private void setupActivity(){
+
         TextView gameButton = findViewById(R.id.gameNav);
         TextView profileButton = findViewById(R.id.highScoreNav);
+
         gameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,6 +99,7 @@ public class UnAuthenticatedProfileActivity extends Activity implements
                 startActivity(intent);
             }
         });
+
         profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -77,41 +109,59 @@ public class UnAuthenticatedProfileActivity extends Activity implements
             }
         });
 
-        final Button loginButton = findViewById(R.id.logInBtn);
-        loginButton.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                SharedPreferences.Editor editor = sharedPrefs.edit();
-                editor.putString("token", "2312312312312");
-                editor.putBoolean("authenticated", true);
-                editor.commit();
-                redirectToAuthenticatedProfile();
-            }
-        });
     }
 
-   private void redirectToAuthenticatedProfile(){
-       Intent intent = new Intent(UnAuthenticatedProfileActivity.this,
-               AuthenticatedProfileActivity.class);
-       finish();
-       startActivity(intent);
-   }
 
-   /* private void signIn() {
+    private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                Toast.makeText(this, "Google sign in failed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                        }
+                    }
+                });
+    }
 
     @Override
     public void onClick(View v) {
         int i = v.getId();
         if (i == R.id.signInButton) {
             signIn();
-        } else if (i == R.id.logInBtn) {
-            signOut();
         }
-    }*/
+    }
 
 }
